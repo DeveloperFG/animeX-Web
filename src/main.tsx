61806291ac8958167a9e0a6c209fad005a0b1165
Fernando Gustavo <fernando.gdev@gmail.com>
2024-11-29 20:58:38 -0300

    ajustando layout 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import App from './App.jsx'
 
-import {store } from '../src/redux/store.tsx'
+import {store } from './redux/store.jsx'
 import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
